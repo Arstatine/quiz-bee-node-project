add79fd9af40a440708ee2b946c88fd9d519b17f
Xenon <x.vergara2201@gmail.com>
2023-01-20 16:42:59 +0800

    Dashboard Search Feature

--- controllers/dashboard/dashboardController.js
+++ controllers/dashboard/dashboardController.js
@@ -11,12 +11,37 @@ const isUserLoggedIn = async (req, res, next) => {
       '_id name email isAdmin avatar'
     );
 
+    if (!findUser.isAdmin) return res.redirect('/home');
+
+    if (!req.query.search) {
+      const findQuestion = await Questions.find(
+        { user_id: _id },
+        '_id user_id title description'
+      ).sort({ createdAt: -1 });
+
+      return res.render('dashboard.ejs', { findUser, findQuestion });
+    }
+
+    let key = req.query.search;
+
     const findQuestion = await Questions.find(
-      { user_id: _id },
+      {
+        $and: [
+          { user_id: _id },
+          {
+            $or: [
+              {
+                title: { $regex: key.toString(), $options: 'i' },
+              },
+              {
+                description: { $regex: key.toString(), $options: 'i' },
+              },
+            ],
+          },
+        ],
+      },
       '_id user_id title description'
     ).sort({ createdAt: -1 });
-
-    if (!findUser.isAdmin) return res.redirect('/home');
 
     return res.render('dashboard.ejs', { findUser, findQuestion });
   } catch (error) {
